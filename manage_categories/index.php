38bb50b617311e660691c93f96e258ddc94e312a
LittleLina93 <ausnijkers@gmail.com>
2022-05-28 00:28:07 +0200

    manage categories

---
+++ manage_categories/index.php
@@ -0,0 +1,55 @@
+<?php
+
+require_once '../helpers/auth-helper.php';
+require './model.php';
+
+init_session();
+
+// Fix here
+if(!isset($_SESSION['categories'])) {
+    $_SESSION['categories'] = [];
+}
+
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    if (isset($_POST['create'])) {
+        $latest = end($_SESSION['categories']);
+        if ($latest) {
+            array_push(
+                $_SESSION['categories'],
+                [ 'id' => ($latest['id'] + 1), 'name' => $_POST['name'] ]
+            );
+        } else {
+            $_SESSION['categories'] = [
+                [ 'id' => 1, 'name' => $_POST['name'] ]
+            ];
+        }
+    }
+    if (isset($_POST['save'])) {
+        // $index = array_search($_POST['id'], $_SESSION['categories']);
+        // var_dump($index);
+        foreach($_SESSION['categories'] as $key => $category) {
+            if (strval($category['id']) === $_POST['id']) {
+                $_SESSION['categories'][$key]['name'] = $_POST['name'];
+
+                // $categories = $_SESSION['categories'];
+                // $current_category = $categories[$key];
+                // $current_category['name'] = $_POST['name'];
+                
+                break;
+            }
+        }
+    }
+
+    if (isset($_POST['delete'])) {
+        
+        function filter_category($category) {
+            return strval($category['id']) !== $_POST['id'];
+        }
+
+        $_SESSION['categories'] = array_filter($_SESSION['categories'], 'filter_category');
+
+    }
+}
+
+require './view.php';
