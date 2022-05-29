f2ec0dc3eff3f0ab206d06181a4c7aa9c244786f
LittleLina93 <ausnijkers@gmail.com>
2022-05-29 21:27:50 +0200

    change_view_accueil

--- accueil/view.php
+++ accueil/view.php
@@ -3,34 +3,38 @@
 <?php $title="Accueil"; $site_description="Accueil"; require "../head.php"; ?>
 <body>
     <?php require "../header.php"; ?>
-<div class="col-lg-9">
-    <div class="filters-content">
-        <div class="row grid">
-            <?php foreach ($newest as $n) : ?>
-            <div class="col-lg-4 col-md-4 col-sm-6 col-xs-12 all des">
-              <div class="item">
-                  <?= $n['title'] ?>
-                  <?= $n['creator'] ?>
-                  <img src="<?=home_path($n['image']) ?>" height="100">
-                  <?= $n['price'] ?>€
-                <a href="images/images.png" data-lightbox="image-1" data-title="Last T-shirt"><img src="images/images.png" alt=""></a>
-              </div>
-            </div>
+
+    <div class="container mt-3">
+        <div class="row">
+            <?php foreach($newest as $n): ?>
+                <div class="col-md-4 mb-4">
+                    <article class="card">
+                      <img src="<?=home_path($n['image']) ?>" alt="Image of <?= $n["title"] ?>"class="card-img-top" />
+                        <div class="card-body">
+                            <h5 class="card-title">"<?= $n['title'] ?>"</h5>
+                            <p class="card-text"><?= $n['creator'] ?></p>
+                            <p class="card-text"><?= $n['price'] ?>€</p>
+                        </div>
+                    </article>
+                </div>
             <?php endforeach; ?>
+    
             <?php foreach ($random as $r) : ?>
-            <div class="col-lg-4 col-md-4 col-sm-6 col-xs-12 all des">
-              <div class="item">
-              <?= $r['title'] ?>
-                  <?= $r['creator'] ?>
-                  <img src="<?=home_path($r['image']) ?>" height="100">
-                  <?= $r['price'] ?>€
-                <a href="images/images.png" data-lightbox="image-1" data-title="Last T-shirt"><img src="images/images.png" alt=""></a>
-              </div>
+              <div class="col-md-4 mb-4">
+                    <article class="card">
+                      <img src="<?=home_path($r['image']) ?>" alt="Image of <?= $r["title"] ?>"class="card-img-top" />
+                        <div class="card-body">
+                            <h5 class="card-title">"<?= $r['title'] ?>"</h5>
+                            <p class="card-text"><?= $r['creator'] ?></p>
+                            <p class="card-text"><?= $n['price'] ?>€</p>
+                        </div>
+                    </article>
+                </div>
+                <?php endforeach; ?>
             </div>
-            <?php endforeach; ?>
+            
         </div>
     </div>
-</div>
     <?php require "../footer.php"; ?>
 </body>
 </html>
